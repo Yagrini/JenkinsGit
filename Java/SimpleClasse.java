db49a7481c3945f59494414f8fc6fcc5ceb94348
Yagrini <lagyouness@gmail.com>
2018-11-15 09:50:37 +0100

    Ajout de la classe SimpleClasse.java

---
+++ Java/SimpleClasse.java
@@ -0,0 +1,10 @@
+public class SimpleClasse{
+    public int somme(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        SimpleClasse simpleClasse = new SimpleClasse();
+        int a=3, b=5;
+        System.out.println("La somme de " + a + " et " + b + " est : " + simpleClasse.somme(a,b) );
+    }
+}
